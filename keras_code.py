cd0c75c961934c36ea7d69d0c3c99d758c7c49ee
bill-cipher <89721184+bill-c1pher@users.noreply.github.com>
2023-02-07 13:12:49 +0530

    Create keras_code.py

---
+++ keras_code.py
@@ -0,0 +1,18 @@
+# Create the model: model
+model = Sequential()
+
+# Add the first hidden layer
+input_shape=(784,)
+model.add(Dense(50, activation='relu',input_shape=input_shape))
+
+# Add the second hidden layer
+model.add(Dense(50, activation='relu', input_shape=input_shape))
+
+# Add the output layer
+model.add(Dense(10, activation='softmax', input_shape=input_shape))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X,y,validation_split=0.3, epochs=10)
